7654d945445829aa67bea6bb5f99a0de64f84ce6
Jacob Sanford <jsanford@unb.ca>
2017-04-19 14:04:06 -0300

    IN-17 Remove install config, copy from core

--- scripts/container/buildDrupalTree.sh
+++ scripts/container/buildDrupalTree.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+
 # Copy build files into a temporary build location.
 mkdir ${DRUPAL_BUILD_TMPROOT}
 cp ${TMP_DRUPAL_BUILD_DIR}/composer.json ${DRUPAL_BUILD_TMPROOT}
@@ -9,8 +10,15 @@ cd ${DRUPAL_BUILD_TMPROOT}
 composer install --${COMPOSER_DEPLOY_DEV}
 
 # Configure scaffolding files.
-ln -s /app/html/vendor/bin/drush /usr/bin/drush
-ln -s /app/html/vendor/bin/drupal /usr/bin/drupal
+ln -s ${DRUPAL_BUILD_TMPROOT}/vendor/bin/drush /usr/bin/drush
+ln -s ${DRUPAL_BUILD_TMPROOT}/vendor/bin/drupal /usr/bin/drupal
+
+# Move profile from repo to build root.
 mv ${TMP_DRUPAL_BUILD_DIR}/${DRUPAL_SITE_ID} ${DRUPAL_BUILD_TMPROOT}/profiles/
+
+# Copy config from standard install profile for current version of Drupal.
+cp -r ${DRUPAL_BUILD_TMPROOT}/core/profiles/standard/config ${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/
+
+# Move settings files into build location.
 mkdir -p ${DRUPAL_BUILD_TMPROOT}/sites/all
 mv ${TMP_DRUPAL_BUILD_DIR}/settings ${DRUPAL_BUILD_TMPROOT}/sites/all/
