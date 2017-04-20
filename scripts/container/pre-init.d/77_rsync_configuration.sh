34106b7ade5e507d3b142049ed2d0157769f307d
Jacob Sanford <jsanford@unb.ca>
2017-04-20 10:18:11 -0300

    IN-20 Replace rsync commands with RSYNC-FLAGS

--- scripts/container/pre-init.d/77_rsync_configuration.sh
+++ scripts/container/pre-init.d/77_rsync_configuration.sh
@@ -8,5 +8,5 @@ fi
 # RSync New Configuration.
 if [ "$DRUPAL_DEPLOY_CONFIGURATION" != "FALSE" ] && [ -d "$TMP_DRUPAL_BUILD_DIR/config-yml" ] && [ "$(ls $TMP_DRUPAL_BUILD_DIR/config-yml)" ]; then
   echo "RSyncing Configuration..."
-  rsync -a --progress --no-perms --no-owner --no-group --delete "$TMP_DRUPAL_BUILD_DIR/config-yml/" "$DRUPAL_CONFIGURATION_DIR/"
+  rsync -a ${RSYNC_FLAGS} --no-perms --no-owner --no-group --delete "$TMP_DRUPAL_BUILD_DIR/config-yml/" "$DRUPAL_CONFIGURATION_DIR/"
 fi
