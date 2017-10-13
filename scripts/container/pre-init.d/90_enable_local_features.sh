92cd0b86314444f806134aceca41e08205fe5b47
Jacob Sanford <jsanford@unb.ca>
2017-10-13 08:19:27 -0300

    IN-61 Force no update E-Mail notifications

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 # Local alterations for your instance.
 # i.e. drush --root=${DRUPAL_ROOT} --uri=default --yes en thirty_two_project
+
+# Squash update emails.
+drush --root=${DRUPAL_ROOT} --uri=default --yes config-set update.settings notification.emails.0 ''
