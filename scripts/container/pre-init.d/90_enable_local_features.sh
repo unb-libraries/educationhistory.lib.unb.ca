87f6b9e1cb32351968c2d50726e5ed0434d89b8a
jsanford <jsanford@unb.ca>
2019-03-14 12:46:48 -0300

    CDOCK-5 Migrate common scripts upstream in pre-init.d

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env sh
-# Local alterations for your instance.
-# i.e. drush --root=${DRUPAL_ROOT} --uri=default --yes en thirty_two_project
-
-# Squash update emails.
-drush --root=${DRUPAL_ROOT} --uri=default --yes config-set update.settings notification.emails.0 ''
+DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
