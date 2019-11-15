2e89bd3ad3452f1caf4c0b70a92e1ab8be695215
Jacob Sanford <jsanford@unb.ca>
2019-11-15 10:42:22 -0400

    DOCKW-5 Tidy up settings files

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
-Include all settings overrides here
-# require_once 'settings_file.inc';
-*/
+/**
+ * @file
+ * Include global settings overrides here.
+ */
 
-// Load environment based includes.
-if (isset($_SERVER['APPLICATION_ENV'])) {
-  $environment = strtolower($_SERVER['APPLICATION_ENV']);
-  $environment_include = dirname(__FILE__) . "/settings.$environment.inc";
-  if (file_exists($environment_include)) {
-    include_once $environment_include;
-  }
-}
-
+// Redis.
 $settings['cache_prefix']['default'] = 'eduhist_';
 $conf['chq_redis_cache_enabled'] = TRUE;
-include_once dirname(__FILE__) . "/settings.redis.inc";
-
-// Add common includes below.
+require_once dirname(__FILE__) . "/settings.redis.inc";
