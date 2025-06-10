4a65a434919cc2170f8aa41147f85878d6684b15
Camilo Villamizar <cvillami@unb.ca>
2025-06-10 14:18:13 -0300

    EH-17 Convert ISSUU PDF display to local embed

--- custom/modules/eh_core/src/Plugin/Block/EhIssuu.php
+++ custom/modules/eh_core/src/Plugin/Block/EhIssuu.php
@@ -21,24 +21,26 @@ class EhIssuu extends BlockBase {
   public function build() {
     $text = '
       <p>
-      Katherine F.C. MacNaughton, M.A. University of New Brunswick Fredericton,
-      New Brunswick, 1947
-      <br>
-      &nbsp;
+        Katherine F.C. MacNaughton, M.A. University of New Brunswick Fredericton,
+        New Brunswick, 1947
+        <br>
+        &nbsp;
       </p>
 
-      <div style="position:relative;padding-top:max(60%,326px);height:0;width:100%"><iframe allow="clipboard-write" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=macn1947&u=unb-etc"></iframe></div>
+      <div class="pdf-container">
+        <embed src="/sites/default/files/images/MacN1947.pdf#view=FitV&amp;zoom=page-height" width="100%" height="600" type="application/pdf">
+      </div>
 
       <p>
-      <br>
-      <a href="/MacNTit">HTML</a>
-      <br>
-      <a href="https://issuu.com/unb-etc/docs/macnaughton?printButtonEnabled=false&amp;shareButtonEnabled=false&amp;searchButtonEnabled=false&amp;backgroundColor=">
-      Page Turner
-      </a>
-      <br>
-      <a href="/sites/default/files/images/MacN1947.pdf">PDF</a><br />
-      <a href="/sites/default/files/2016-07/education_history.epub">ePub (for e-readers)</a>
+        <br>
+        <a href="/MacNTit">HTML</a>
+        <br>
+        <a href="https://issuu.com/unb-etc/docs/macnaughton?printButtonEnabled=false&amp;shareButtonEnabled=false&amp;searchButtonEnabled=false&amp;backgroundColor=">
+        Page Turner
+        </a>
+        <br>
+        <a href="/sites/default/files/images/MacN1947.pdf">PDF</a><br />
+        <a href="/sites/default/files/2016-07/education_history.epub">ePub (for e-readers)</a>
       </p>
     ';
 
