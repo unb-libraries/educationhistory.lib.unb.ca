unb-libraries/exhibits.lib.unb.ca

Migrate LABEL in Dockerfile [skip ci]

Dockerfile
@@ -41,16 +41,12 @@ ARG BUILD_DATE
 ARG VCS_REF
 ARG VERSION
 LABEL ca.unb.lib.generator="omeka" \
-      com.microscaling.docker.dockerfile="/Dockerfile" \
-      com.microscaling.license="MIT" \
-      org.label-schema.build-date=$BUILD_DATE \
-      org.label-schema.description="exhibits.lib.unb.ca provides moderate scale digital exhibits at UNB Libraries." \
-      org.label-schema.name="exhibits.lib.unb.ca" \
-      org.label-schema.schema-version="1.0" \
-      org.label-schema.url="https://exhibits.lib.unb.ca" \
-      org.label-schema.vcs-ref=$VCS_REF \
-      org.label-schema.vcs-url="https://github.com/unb-libraries/exhibits.lib.unb.ca" \
-      org.label-schema.vendor="University of New Brunswick Libraries" \
-      org.label-schema.version=$VERSION \
+      org.opencontainers.image.title="exhibits.lib.unb.ca" \
+      org.opencontainers.image.description="exhibits.lib.unb.ca provides moderate scale digital exhibits at UNB Libraries." \
+      org.opencontainers.image.vendor="University of New Brunswick Libraries" \
       org.opencontainers.image.authors="UNB Libraries <libsupport@unb.ca>" \
-      org.opencontainers.image.source="https://github.com/unb-libraries/exhibits.lib.unb.ca"
+      org.opencontainers.image.url="https://exhibits.lib.unb.ca" \
+      org.opencontainers.image.source="https://github.com/unb-libraries/exhibits.lib.unb.ca" \
+      org.opencontainers.image.version="$VERSION" \
+      org.opencontainers.image.revision="$VCS_REF" \
+      org.opencontainers.image.created="$BUILD_DATE"

unb-libraries/educationhistory.lib.unb.ca

Dockerfile
@@ -27,16 +27,12 @@ ARG BUILD_DATE
 ARG VCS_REF
 ARG VERSION
 LABEL ca.unb.lib.generator="drupal11" \
-  com.microscaling.docker.dockerfile="/Dockerfile" \
-  com.microscaling.license="MIT" \
-  org.label-schema.build-date=$BUILD_DATE \
-  org.label-schema.description="educationhistory.lib.unb.ca provides web-readble copies of the Development of the Theory and Practice of Education in New Brunswick at UNB Libraries." \
-  org.label-schema.name="educationhistory.lib.unb.ca" \
-  org.label-schema.schema-version="1.0" \
-  org.label-schema.url="https://educationhistory.lib.unb.ca" \
-  org.label-schema.vcs-ref=$VCS_REF \
-  org.label-schema.vcs-url="https://github.com/unb-libraries/educationhistory.lib.unb.ca" \
-  org.label-schema.vendor="University of New Brunswick Libraries" \
-  org.label-schema.version=$VERSION \
+  org.opencontainers.image.title="educationhistory.lib.unb.ca" \
+  org.opencontainers.image.description="educationhistory.lib.unb.ca provides web-readble copies of the Development of the Theory and Practice of Education in New Brunswick at UNB Libraries." \
+  org.opencontainers.image.vendor="University of New Brunswick Libraries" \
   org.opencontainers.image.authors="UNB Libraries <libsupport@unb.ca>" \
-  org.opencontainers.image.source="https://github.com/unb-libraries/educationhistory.lib.unb.ca"
+  org.opencontainers.image.url="https://educationhistory.lib.unb.ca" \
+  org.opencontainers.image.source="https://github.com/unb-libraries/educationhistory.lib.unb.ca" \
+  org.opencontainers.image.version="$VERSION" \
+  org.opencontainers.image.revision="$VCS_REF" \
+  org.opencontainers.image.created="$BUILD_DATE"