efed1446ab48066c39ce343984f937746ad3e13c
Camilo Villamizar <cvillami@unb.ca>
2026-09-10 16:21:52 -0300

    IN-398 Increase PDF viewer height

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,7 @@ useHead({ title: 'Home' })
   <main>
     <h2>The Development of the Theory and Practice of Education in New Brunswick, 1784-1900: A Study in Historical Background</h2>
     <p>Katherine F.C. MacNaughton, M.A. University of New Brunswick Fredericton, New Brunswick, 1947</p>
-    <embed src="/files/MacN1947.pdf#view=FitV&amp;zoom=page-height" width="100%" height="600" type="application/pdf">
+    <embed src="/files/MacN1947.pdf#view=FitV&amp;zoom=page-height" width="100%" height="800" type="application/pdf">
     <h3>Downloads</h3>
     <ul class="download-list">
       <li><a href="/book/title-page">HTML</a></li>
